c594f04dae3aaa8bc1b65d99f8a05d6a7c33e619
Kareem <kpjackson27@hotmail.com>
2015-05-05 03:55:45 -0400

    front page 2

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,4 +1,5 @@
-var keystone = require('keystone');
+var keystone = require('keystone'),
+	async = require('async');
 
 exports = module.exports = function(req, res) {
 	
@@ -8,6 +9,8 @@ exports = module.exports = function(req, res) {
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'home';
+
+	view.query('posts', keystone.list('Post').model.find().limit(4).sort('sortOrder'));
 	
 	// Render the view
 	view.render('index');
